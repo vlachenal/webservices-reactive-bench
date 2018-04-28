5887150ac48ef5d622ab17c854270a7a8d89fc78
Vincent Lachenal <vincent.lachenal@gmail.com>
2018-04-28 12:55:47 +0200

    javadoc

--- src/test/java/com/github/vlachenal/webservice/reactive/bench/RequestDataSourceCI.java
+++ src/test/java/com/github/vlachenal/webservice/reactive/bench/RequestDataSourceCI.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.PropertySource;
 
 
 /**
- * Datasource configuration
+ * Datasource configuration for continous integration
  *
  * @author Vincent Lachenal
  */
